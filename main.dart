52ac82f0bb42993df167563f6a3b3b1ee50ba62f
Nourhannnn7 <150105825+Nourhannnn7@users.noreply.github.com>
2023-11-30 14:06:06 +0200

    Add files via upload

--- main.dart
+++ main.dart
@@ -1,36 +1,125 @@
 import 'package:flutter/material.dart';
-  void main ()
-  {
-    runApp(MyApp());
-  }
-      class MyApp extends StatelessWidget {
+
+void main() {
+  runApp(MyApp());
+}
+
+class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
-          return MaterialApp(
-            theme: ThemeData.dark(),
-              home: Scaffold(
-                appBar:  AppBar(title: const Text("CyberGurad"), titleTextStyle: const TextStyle(
-                  fontWeight: FontWeight.normal, fontSize: 21, color: Color(0xffe2004f),
-                                                     ),),
-                body: Row(
-                    crossAxisAlignment:CrossAxisAlignment.center,
-                    mainAxisAlignment: MainAxisAlignment.start,
-                    children: [
-
-                    Text("Speak Up!, We Hear You." , style: TextStyle(
-
-                    ),),
-                    Image.network("https://muslimgirl.com/wp-content/uploads/2016/11/social-media-abuse.jpg" , width: 250,
-                    )
-
+    return MaterialApp(
+      home: Scaffold(
+        appBar: AppBar(
+          title: const Text("Graduation Project"),
+        ),
+        body: Container(
+          padding: const EdgeInsets.all(15),
+          child: Column(
+            children: [
+              Container(
+                decoration: BoxDecoration(
+                  border: Border.all(color:const  Color(0xff3a5985), width: 3,),
+                  borderRadius: BorderRadius.circular(19),
+                  color: const Color(0xffb2caee),
+                ),
+                margin: const EdgeInsets.all(10),
+                width: 380,
+                padding: const EdgeInsets.all(13),
+                alignment: Alignment.center,
+                child:const  Text(
+                  "Secure Shield",
+                  textAlign: TextAlign.center,
+                  style: TextStyle(
+                    fontWeight: FontWeight.w900,
+                    fontSize: 20,
+                    color: Color(0xff618fca),
+                  ),
+                ),
+              ),
+              Container(
+                decoration: BoxDecoration(
+                  border: Border.all(color:const  Color(0xff3a5985), width: 3,),
+                  borderRadius: BorderRadius.circular(19),
+                  color:const  Color(0xffb2caee),
+                ),
+                width: 362,
+                padding: const EdgeInsets.all(13),
+                alignment: Alignment.center,
+                child: const Text(
+                  "Our application targets female students in Egyptian Universities especially KafrElsheik, Our purpose is to combat electronic blackmail against women.",
+                  textAlign: TextAlign.center,
+                  style: TextStyle(
+                    fontWeight: FontWeight.w900,
+                    fontSize: 15,
+                    color: Color(0xff618fca),
+                  ),
+                ),
+              ),
+              Container(
+                decoration: BoxDecoration(
+                  border: Border.all(color: const Color(0xff3a5985), width: 3,),
+                  borderRadius: BorderRadius.circular(19),
+                  color:const  Color(0xffb2caee),
+                ),
+                width: 380,
+                margin:const  EdgeInsets.all(10),
+                padding: const EdgeInsets.all(13),
+                alignment: Alignment.center,
+                child: const Row(
+                  mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+                  children: [
+                     Row(
+                      children: [
+                         const Icon(Icons.star, size: 25),
+                         const Icon(Icons.star, size: 25),
+                        const Icon(Icons.star, size: 25),
+                        const Icon(Icons.star, size: 25),
+                        const Icon(Icons.star, size: 25),
+                      ],
+                    ),
+                    const Spacer(), // Add Spacer widget to create space
+                    const Text("40 Reviews", style:
+                    const TextStyle(fontSize: 18), ),
                   ],
                 ),
-
-
-
-              ));
-
-
-
-    }
-    }
+              ),
+              Container(
+                width: 359,
+                padding: const EdgeInsets.all((10)),
+                decoration: BoxDecoration(
+                  border: Border.all(color: const Color(0xff3a5985), width: 3,),
+                  borderRadius: BorderRadius.circular(19),
+                  color: const Color(0xffb2caee),
+                ),
+                    child: const Row(
+                      mainAxisAlignment: MainAxisAlignment.spaceAround,
+                      children: [
+                        Column(
+                          children: const [
+                             Icon(Icons.add_call),
+                            Text("Hotline 07775000", style: TextStyle(color: Color(0xff618fca), fontWeight: FontWeight.w900 ),),
+                            
+                          ],
+                        ),
+                        Column(
+                          children: [
+                            Icon(Icons.add),
+                            Text("FB: Secure Shield", style: TextStyle(color: Color(0xff618fca), fontWeight: FontWeight.w900),)
+                          ],
+                        ),
+                        Column(
+                          children: [
+                            Icon(Icons.adb_sharp),
+                            Text("Chat with us", style: TextStyle(color: Color(0xff618fca), fontWeight: FontWeight.w900), )
+                          ],
+                        )
+                      ],
+                    ),
+              ),
+            ],
+          ),
+        ),
+      ),
+    );
+  }
+}
